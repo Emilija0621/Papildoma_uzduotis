a1231e3fadff5603e8ca6c91a10d7495ff72c859
Emilija0621 <emilija.adomonyte@gmail.com>
2025-12-13 21:44:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,26 +9,55 @@ using std::string;
 using std::map;
 using std::endl;
 using std::cin;
+using std::invalid_argument;
 
 
-int main(){
-    
-    string failo_pavadinimas;
-    
-    cout << "Iveskite failo pavadinima: "; cin >> failo_pavadinimas;
-    vector<string> eilutes = txt_skaitymas(failo_pavadinimas);
-    
-    map<string, Zodziai> zodziu_duomenys = zodziu_skaiciavimas(eilutes);
-    
-    for (const auto& [zodis, duomenys] : zodziu_duomenys) {
-            if (duomenys.count > 1) {
-                cout << zodis << " kartų: " << duomenys.count << ", eilutės: ";
-                for (int nr : duomenys.eilutes_nr) {
-                    cout << nr << " ";
+int main() {
+    while (true) {
+        string failo_pavadinimas;
+        vector<string> eilutes;
+        
+        while (true) {
+            cout << "Įveskite failo pavadinimą: ";
+            cin >> failo_pavadinimas;
+            
+            eilutes = failo_skaitymas(failo_pavadinimas);
+            if (!eilutes.empty()) break;
+            cout << "Bandykite dar kartą." << endl;
+        }
+        
+        map<string, Zodziai> zodziai = zodziu_skaiciavimas(eilutes);
+        
+        int pasirinkimas = 0;
+        while (true) {
+            cout << "Kaip norite gauti rezultatus?" << endl;
+            cout << "1 - į konsolę" << endl;
+            cout << "2 - į failą" << endl;
+            
+            string ivestis;
+            cin >> ivestis;
+            
+            try {
+                pasirinkimas = stoi(ivestis);
+                if (pasirinkimas < 1 || pasirinkimas > 2) {
+                    cout << "Neteisingas pasirinkimas. Įveskite skaičių nuo 1 iki 2." << endl;
+                    continue;
                 }
-                cout << endl;
+                break;
+            } catch (const invalid_argument&) {
+                cout << "Įvesta netinkama reikšmė. Įveskite skaičių nuo 1 iki 2." << endl;
             }
         }
-
-        return 0;
+        
+        if (pasirinkimas == 1) {
+            cross_reference_terminalas(zodziai);
+        } else if (pasirinkimas == 2) {
+            string failo_isvestis;
+            cout << "Įveskite išvesties failo pavadinimą: ";
+            cin >> failo_isvestis;
+            cross_reference_i_faila(zodziai, failo_isvestis);
+            cout << "Lentelė išsaugota faile: " << failo_isvestis << endl;
+        }
+    }
+    return 0;
 }
