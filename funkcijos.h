12da8b7a8d1161eadca9c5ffaacf84a4744136f2
Emilija0621 <emilija.adomonyte@gmail.com>
2025-12-15 19:43:32 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -3,17 +3,15 @@
 #include <string>
 #include <map>
 
-#include "zodziai.h"
 
 using std::string;
 using std::vector;
 using std::map;
-
-string sutvarkyti_zodziai(const string& zodis);
-vector<string> failo_skaitymas(const string& failo_pavadinimas);
-map<string, Zodziai> zodziu_skaiciavimas(const vector<string>& eilutes);
-void cross_reference_terminalas(const map<string, Zodziai>& zodziai);
-void cross_reference_i_faila(const map<string, Zodziai>& zodziai, const string& failo_vardas);
+using std::wstring;
 
 
-
+wstring sutvarkyti_zodziai(const wstring& zodis);
+vector<wstring> failo_skaitymas(const string& failo_pavadinimas);
+void cross_reference_terminalas(const map<wstring, int>& zodziu_skaicius, const map<wstring, vector<int>>& zodziu_eilutes);
+void cross_reference_i_faila(const map<wstring, int>& zodziu_skaicius, const map<wstring, vector<int>>& zodziu_eilutes, const string& failo_vardas);
+void analizuoti_teksta(const vector<wstring>& eilutes, map<wstring, int>& zodziu_skaicius, map<wstring, vector<int>>& zodziu_eilutes);
