be372986a3e4cc0d21a4cb0ab14ac8ca2c10c709
Emilija0621 <emilija.adomonyte@gmail.com>
2025-12-23 00:22:26 +0200

    Update and rename funkcijos.h to zodziai.h

--- zodziai.h
+++ zodziai.h
@@ -3,14 +3,11 @@
 #include <string>
 #include <map>
 
-
 using std::string;
+using std::wstring;
 using std::vector;
 using std::map;
-using std::wstring;
 
-
-wstring sutvarkyti_zodziai(const wstring& zodis);
 vector<wstring> failo_skaitymas(const string& failo_pavadinimas);
 void cross_reference_terminalas(const map<wstring, int>& zodziu_skaicius, const map<wstring, vector<int>>& zodziu_eilutes);
 void cross_reference_i_faila(const map<wstring, int>& zodziu_skaicius, const map<wstring, vector<int>>& zodziu_eilutes, const string& failo_vardas);
